2a0de3a05f20120f8d35c9184a851f9dd8822b8b
Riko Jacob (@ITU Air 13) <rikj@itu.dk>
2017-02-06 22:17:07 +0100

    post-commit: touch changed files, triggering make

--- createpdf.sh
+++ createpdf.sh
@@ -24,9 +24,9 @@ for pdfname in $*; do
         if [ -z "$GITSTATUS" ]; then
             echo uploading $basename
             curl -s -u $BITBUCKETLOGIN -X POST "https://api.bitbucket.org/2.0/repositories/rikj/bads-labs/downloads" -F files=@"$basename.pdf"
-        else
-            touch -m -r "$basename.tex" "$basename.pdf"
-            touch -A -01 "$basename.pdf"
+        #else
+            #touch -m -r "$basename.tex" "$basename.pdf"
+            #touch -A -01 "$basename.pdf"
         fi
     fi
     )
